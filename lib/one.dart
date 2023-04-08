6f997012b9fc745aa341d2a836141403bd402ad7
Ripple <pikarupika7@gmail.com>
2023-04-08 19:10:42 +0545

    initial commit

---
+++ lib/one.dart
@@ -0,0 +1,13 @@
+//WAP to check if the number is odd or even
+
+void main() {
+  oddEven();
+}
+
+void oddEven(int num) {
+  if (num % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
---
+++ lib/two.dart
@@ -0,0 +1,12 @@
+//wap to check whether a character is a vowel or consonant character
+void main(){
+  String ch = "a";
+}
+void check(){
+  var ch;
+  if(ch=="a" || ch=="e" || ch=="i" || ch=="o" || ch=="u"){
+    print("Vowel");
+  }else{
+    print("Consonant");
+  }
+}
